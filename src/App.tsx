f7d374a1115a94de562bde9fd1c8c839d8db48e6
redreev8 <redreevs@mail.ru>
2024-12-18 17:42:17 +0500

    formik-yup: add form room

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Flex } from '@chakra-ui/react'
 import Form from './components/form'
+import FormRoom from './components/form-room'
 
 const App = () => {
 	return (
 		<Container maxW="sm" px="2" py="4">
-			<Form />
+			<Flex direction="column" gap="20">
+				<Form />
+				<FormRoom />
+			</Flex>
 		</Container>
 	)
 }
